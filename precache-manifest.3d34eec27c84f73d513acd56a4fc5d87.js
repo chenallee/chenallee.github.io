6afc2018d3e4064a4258345d0433d4c8d7890615
chenallee <lchenalloy@gmail.com>
2020-05-18 21:04:25 -0400

    Updates

---
+++ precache-manifest.3d34eec27c84f73d513acd56a4fc5d87.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16af1c9e7889e444691404fd62c630e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a076054724a27850402",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "687db54576f52878c9b4",
+    "url": "/static/css/main.b621bf0f.chunk.css"
+  },
+  {
+    "revision": "3a076054724a27850402",
+    "url": "/static/js/2.1fe56e60.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.1fe56e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687db54576f52878c9b4",
+    "url": "/static/js/main.aefd750e.chunk.js"
+  },
+  {
+    "revision": "c965741ab3b0b07f0126",
+    "url": "/static/js/runtime-main.c5340adb.js"
+  },
+  {
+    "revision": "6c9596f9e2774d853601784fbbb2dc0d",
+    "url": "/static/media/0.6c9596f9.jpg"
+  },
+  {
+    "revision": "3fe05d7cbaf6844e3c145342df5bde42",
+    "url": "/static/media/Resume.3fe05d7c.pdf"
+  }
+]);

bf1abc4a3b50c276768e37afbe60b39b9aa0150e
chenallee <lchenalloy@gmail.com>
2020-05-26 17:38:50 -0400

    transferring fix made from repo to here

---
+++ src/components/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Footer() {
+
+    return (
+        <Navbar variant='dark' bg='' className='footer bodyfont' style={{ backgroundColor: 'transparent' }}>
+            <Container fluid className='d-flex flex-column justify-content-center'>
+            <Navbar.Text className='toTop'>
+                    <a href='#' className='iconLink' onClick={() => {
+                        window.scrollTo({
+                            top: -2,
+                            behavior: 'smooth'
+                        })
+                    }}>
+                    <FontAwesomeIcon icon='angle-up' size='3x'  />
+                    </a>
+                </Navbar.Text>
+                <Navbar.Text >
+                    <h4>
+                        Contact me!
+                    </h4>
+                </Navbar.Text>
+                <Navbar.Text>
+                    <a href='https://github.com/chenallee' target='_blank' rel="noopener noreferrer" className='mr-3 iconLink'>
+                        <FontAwesomeIcon icon={['fab', 'github-square']} size='3x' />
+                    </a>
+                    <a href='https://linkedin.com/in/lee-chenalloy' target='_blank' rel="noopener noreferrer" className='ml-3 mr-3 iconLink'>
+                        <FontAwesomeIcon icon={['fab', 'linkedin']} size='3x' />
+                    </a>
+                    <a href='mailto:lchenalloy@gmail.com' target='_blank' rel="noopener noreferrer" className='ml-3 iconLink'>
+                        <FontAwesomeIcon icon='envelope-square' size='3x' />
+                    </a>
+                </Navbar.Text>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Footer;
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+
+import projectList from '../../assets/projects.json';
+import Project from '../Project';
+
+import '../Projects.css';
+
+function Projects() {
+  const [projects] = useState(projectList);
+
+  return (
+    <>
+     <Container className='content'>
+       <div className=''>
+       <h1>projects</h1>
+       <p>Check out some of my work! I have experience with ES6 syntax and the MERN stack. Currently working on mastering CSS, client-side JavaScript, and front end tools.</p>
+       </div>
+       
+       <div>
+        {/* for each project, return a project component with the appropriate information */}
+        {projects.map((project) => {
+          return (
+            <Project 
+            key={project.id}
+            title={project.title}
+            desc={project.description}
+            livelink={project.livelink}
+            codelink={project.codelink}
+            technologies={project.technologies}
+            img={project.img}
+             />
+          );
+        })}
+     </div>
+
+     </Container>
+     
+           
+    </>
+  );
+}
+
+export default Projects;
